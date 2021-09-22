5d004307c0b99c785def45570f32c9d833093bef
彭成刚 <76526211@qq.com>
2021-09-22 19:31:01 +0800

    up

---
+++ public_web/frame/main1data.js
@@ -0,0 +1,303 @@
+var list = [
+  {
+    name: "博客园",
+    href: "http://www.cnblogs.com/",
+    src: "../images/main1/bokeyuan.png",
+  },
+  {
+    name: "百度脑图",
+    href: "http://naotu.baidu.com/",
+    src: "../images/main1/baidunaotu.jpg",
+  },
+  {
+    name: "processOn",
+    href: "https://www.processon.com/",
+    src: "../images/main1/process.jpg",
+  },
+  {
+    name: "Cmd markdown",
+    href: "https://www.zybuluo.com/cmd/",
+    src: "../images/main1/cmdmarkdown.jpg",
+  },
+  {
+    name: "哔哩哔哩弹幕",
+    href: "https://www.processon.com/",
+    src: "../images/main1/bilibili.jpg",
+  },
+  {
+    name: "新浪微博",
+    href: "http://weibo.com/pengchenggang",
+    src: "../images/main1/sinaweibo.png",
+  },
+  {
+    name: "美编N1人人小站",
+    href: "http://zhan.renren.com/meibiann1",
+    src: "../images/main1/meibiann1xiaozhan.png",
+  },
+  {
+    name: "淘宝搜索",
+    href: "http://search.taobao.com/",
+    src: "../images/main1/taobaosearch.png",
+  },
+  {
+    name: "薇拉摄影",
+    href: "http://www.iaviva.com/",
+    src: "../images/main1/iaviva.png",
+  },
+  {
+    name: "新浪微博搜索",
+    href: "http://s.weibo.com/",
+    src: "../images/main1/sweibo.png",
+  },
+  {
+    name: "iu最酷榜",
+    href: "http://www.youku.com/show_page/id_z609bbc242e2511e29498.html",
+    src: "../images/main1/iuzuiku.png",
+  },
+  {
+    name: "zealer中国 王自如",
+    href: "http://www.zealer.com/",
+    src: "../images/main1/zealer.png",
+  },
+  {
+    name: "美食天下",
+    href: "http://www.meishichina.com/",
+    src: "../images/main1/meishitianxia.png",
+  },
+  {
+    name: "美食杰",
+    href: "http://www.meishij.net/",
+    src: "../images/main1/meishijie.png",
+  },
+  {
+    name: "程序员导航",
+    href: "http://www.pg265.com/",
+    src: "../images/main1/pg265.png",
+  },
+  {
+    name: "好123",
+    href: "http://www.hao123.com/",
+    src: "../images/main1/hao123.png",
+  },
+  {
+    name: "虎嗅",
+    href: "http://www.huxiu.com/",
+    src: "../images/main1/huxiu.png",
+  },
+  {
+    name: "2345网址导航",
+    href: "http://www.2345.com/",
+    src: "../images/main1/2345.png",
+  },
+  { name: "猫扑", href: "http://www.mop.com/", src: "../images/main1/mop.png" },
+  {
+    name: "天涯",
+    href: "http://focus.tianya.cn/",
+    src: "../images/main1/tianya.png",
+  },
+  {
+    name: "百度云网盘",
+    href: "http://pan.baidu.com/",
+    src: "../images/main1/baiduyunbox.png",
+  },
+  {
+    name: "新浪V盘",
+    href: "http://vdisk.weibo.com",
+    src: "../images/main1/xinlangvpan.png",
+  },
+  {
+    name: "善用佳软",
+    href: "http://xbeta.info/",
+    src: "../images/main1/xbeta.png",
+  },
+  {
+    name: "小众软件",
+    href: "http://www.appinn.com/",
+    src: "../images/main1/appinn.png",
+  },
+  {
+    name: "精品绿色便携软件",
+    href: "http://www.portablesoft.org/",
+    src: "../images/main1/lvsebianxieruanjian.png",
+  },
+  {
+    name: "异次元软件世界",
+    href: "http://www.iplaysoft.com/",
+    src: "../images/main1/yiciyuan.png",
+  },
+  {
+    name: "邻居的耳朵",
+    href: "http://kxt.fm/",
+    src: "../images/main1/erduo.png",
+  },
+  {
+    name: "百度随心听",
+    href: "http://fm.baidu.com/",
+    src: "../images/main1/fmbaidu.png",
+  },
+  {
+    name: "36氪",
+    href: "http://www.36kr.com/",
+    src: "../images/main1/36kr.png",
+  },
+  {
+    name: "蓝色理想",
+    href: "http://www.blueidea.com/",
+    src: "../images/main1/blueidea.png",
+  },
+  {
+    name: "AE视频教程",
+    href: "http://www.51zxw.net/list.aspx?cid=316",
+    src: "../images/main1/aejiaocheng.png",
+  },
+  {
+    name: "鲜果网",
+    href: "http://xianguo.com/",
+    src: "../images/main1/xianguo.png",
+  },
+  {
+    name: "煎蛋网",
+    href: "http://jandan.net/",
+    src: "../images/main1/jiandan.png",
+  },
+  {
+    name: "iPc.me",
+    href: "http://www.ipc.me/",
+    src: "../images/main1/ipcme.png",
+  },
+  {
+    name: "QQ邮箱",
+    href: "http://mail.qq.com/",
+    src: "../images/main1/mailqq.png",
+  },
+  {
+    name: "thinkphp",
+    href: "http://www.thinkphp.cn/",
+    src: "../images/main1/thinkphp.png",
+  },
+  {
+    name: "HP孙振耀退休感言",
+    href: "http://www.douban.com/note/205094050/",
+    src: "../images/main1/mimang.png",
+  },
+  {
+    name: "微电影实时分享",
+    href: "http://www.vmovier.com/",
+    src: "../images/main1/vmovier.png",
+  },
+  {
+    name: "火星网",
+    href: "http://www.hxsd.com/",
+    src: "../images/main1/huoxing.png",
+  },
+  {
+    name: "旅行不是济世良药",
+    href: "http://www.douban.com/note/262479071/",
+    src: "../images/main1/lvxing.png",
+  },
+  {
+    name: "什么是执行力",
+    href: "http://blog.sina.com.cn/s/blog_49dc168d0101a5iw.html",
+    src: "../images/main1/zhixingli.png",
+  },
+  {
+    name: "网易云课堂",
+    href: "http://study.163.com/u/pengchenggang",
+    src: "../images/main1/wangyiyunketang.png",
+  },
+  {
+    name: "维基百科",
+    href: "http://zh.wikipedia.org",
+    src: "../images/main1/weijibaike.png",
+  },
+  {
+    name: "美味爱读",
+    href: "http://readwise.net/",
+    src: "../images/main1/meiweiaidu.png",
+  },
+  {
+    name: "网易摄影",
+    href: "http://pp.163.com/square",
+    src: "../images/main1/wangyisheying.png",
+  },
+  {
+    name: "色影无忌",
+    href: "http://ww.xitek.com/",
+    src: "../images/main1/seyingwuji.png",
+  },
+  {
+    name: "威锋网",
+    href: "http://www.weiphone.com/",
+    src: "../images/main1/weiphone.png",
+  },
+  {
+    name: "中关村在线",
+    href: "http://www.zol.com.cn/",
+    src: "../images/main1/zol.png",
+  },
+  {
+    name: "放放电影",
+    href: "http://www.poxiao.com/",
+    src: "../images/main1/fangfang.png",
+  },
+  {
+    name: "稚子春泥儿童摄影",
+    href: "http://zzcn.pp.163.com/",
+    src: "../images/main1/zhizichunni.png",
+  },
+  {
+    name: "即刻搜索",
+    href: "http://www.jike.com/",
+    src: "../images/main1/jike.png",
+  },
+  {
+    name: "天津每日房价",
+    href: "http://www2.tjfdc.gov.cn/pages/efwgl.aspx?id=gterqi_fwglmrfj",
+    src: "../images/main1/tjfdcgovcn.png",
+  },
+  {
+    name: "人人影视·美剧",
+    href: "http://www.yyets.com/",
+    src: "../images/main1/renrenyingshi.png",
+  },
+  {
+    name: "折页",
+    href: "http://zheye.cc/",
+    src: "../images/main1/zheye.cc.png",
+  },
+  {
+    name: "奇酷",
+    href: "http://www.qikoo.com/",
+    src: "../images/main1/qikoo.png",
+  },
+  {
+    name: "jing.Fm",
+    href: "http://jing.fm/",
+    src: "../images/main1/jingfm.jpg",
+  },
+  {
+    name: "迅雷电影院",
+    href: "http://vip.kankan.com/",
+    src: "../images/main1/vipkankan.jpg",
+  },
+  {
+    name: "青岛旅游",
+    href: "http://sinaleju.sinaapp.com/other/qdlvyou/index.html",
+    src: "../images/main1/qingdaolvyou.jpg",
+  },
+  {
+    name: "微信公众平台",
+    href: "https://mp.weixin.qq.com/",
+    src: "../images/main1/mpweixin.jpg",
+  },
+  {
+    name: "喜马拉雅",
+    href: "http://www.ximalaya.com/",
+    src: "../images/main1/ximalaya.jpg",
+  },
+  {
+    name: "微信搜索",
+    href: "http://weixin.sogou.com/",
+    src: "../images/main1/weixinsousuo.jpg",
+  },
+];
