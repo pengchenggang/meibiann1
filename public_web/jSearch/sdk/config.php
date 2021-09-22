d79738cf52afd7b2548a9ab2cef859d698a0e4b6
彭成刚 <76526211@qq.com>
2021-09-22 17:31:42 +0800

    del

--- public_web/jSearch/sdk/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-
-define( "WB_AKEY" , '179496283' );
-define( "WB_SKEY" , 'f63bd37b92dfbf9089c3326083f4aa12' );
-define( "WB_CALLBACK_URL" , 'http://sinaleju.sinaapp.com/jSearch/sdk/callback.php' );
