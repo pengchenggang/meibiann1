4755e37d32b06bf4730eeb8ec2423d1c77cb6954
彭成刚 <76526211@qq.com>
2021-09-22 19:35:34 +0800

    up

---
+++ public_web/frame/main9data.js
@@ -0,0 +1,182 @@
+var list = [
+  {
+    name: "中年人的彷徨谁懂",
+    href: "https://v.qq.com/x/page/u0786rrj9wc.html?start=0",
+    src: "../images/main9/panghuang.jpg",
+  },
+  {
+    name: "2013设计部拜年",
+    href: "http://v.youku.com/v_show/id_XNTQzMTUwODk2.html",
+    src: "../images/main9/2013bainian.png",
+  },
+  {
+    name: "2014设计部拜年",
+    href: "http://v.youku.com/v_show/id_XNjY1NzUxNTIw.html",
+    src: "../images/main9/2014sjbbainian.png",
+  },
+  {
+    name: "2015新征程 设计部",
+    href: "http://v.qq.com/boke/page/q/4/s/q0147v9f14s.html",
+    src: "../images/main9/2015xinzhengcheng.jpg",
+  },
+  {
+    name: "2015以终为始",
+    href: "http://v.qq.com/page/e/g/u/e018378yggu.html",
+    src: "../images/main9/2015yzws.jpg",
+  },
+  {
+    name: "一分钟学区房",
+    href: "http://v.qq.com/page/l/i/m/l01514t2fim.html",
+    src: "../images/main9/xuequfang.jpg",
+  },
+  {
+    name: "Everything模式",
+    href: "https://v.youku.com/v_show/id_XNDQ3MTExMTc2.html",
+    src: "../images/main9/everything.png",
+  },
+  {
+    name: "2013&middot;315交房骗局",
+    href: "http://tj.house.sina.com.cn/zhuanti/2013315jiaofangpianju/index.shtml",
+    src: "../images/main9/jiaofangpianju3152013.png",
+  },
+  {
+    name: "父爱无疆",
+    href: "http://tj.house.sina.com.cn/zhuanti/201206fuaiwujiang/index.shtml",
+    src: "../images/main9/fuaiwujiang.png",
+  },
+  {
+    name: "2012&middot;315白金骗局",
+    href: "http://weibo.com/1239160461/y9OPOgXfT",
+    src: "../images/main9/baijinpianju.png",
+  },
+  {
+    name: "2012乐居视频总结",
+    href: "http://tj.house.sina.com.cn/zhuanti/201301nzzjshipinpian/index.shtml",
+    src: "../images/main9/video2012.png",
+  },
+  {
+    name: "乐棠一岁MV",
+    href: "http://weibo.com/1239160461/zcXZqz9SJ",
+    src: "../images/main9/letang1mv.png",
+  },
+  {
+    name: "乐棠两岁超长MV",
+    href: "http://v.youku.com/v_show/id_XNjYzNDQ0MjY4.html",
+    src: "../images/main9/2smv.png",
+  },
+  {
+    name: "奥林匹克森林公园",
+    href: "http://v.youku.com/v_show/id_XNzMwNTQ2NTA0.html",
+    src: "../images/main9/aolinpike.jpg",
+  },
+  {
+    name: "看房团工作篇",
+    href: "http://video.sina.com.cn/v/b/100080095-1322343775.html",
+    src: "../images/main9/gongzuopian.png",
+  },
+  {
+    name: "手指舞视频",
+    href: "http://www.tangdou.com/v45/dANQMMNjwD1TzQ2.html",
+    src: "../images/main9/shouzhiwu.png",
+  },
+  {
+    name: "AE制作企业宣传",
+    href: "http://www.56.com/u92/v_OTU2Nzk5Mjk.html",
+    src: "../images/main9/qiyexuanchuan.png",
+  },
+  {
+    name: "韩国创意广告",
+    href: "http://www.iqiyi.com/v_19rrgjahkg.html",
+    src: "../images/main9/hgchuangyigg.png",
+  },
+  {
+    name: "2013年广告欣赏",
+    href: "http://m.56.com/view/id-ODYyNDc4NTk.html/1030_cdcly21.html",
+    src: "../images/main9/2013guanggaochuan.png",
+  },
+  {
+    name: "梦幻音乐 逐格动画",
+    href: "http://v.youku.com/v_show/id_XNDgwNDY5MzQw.html",
+    src: "../images/main9/menghuanyinyue.png",
+  },
+  {
+    name: "Fight For Everyone",
+    href: "http://v.youku.com/v_show/id_XNTI4NTY2MTk2.html",
+    src: "../images/main9/shangdizhishou.png",
+  },
+  {
+    name: "两分钟了解任天堂",
+    href: "http://v.youku.com/v_show/id_XNDk5NTQyNzky.html",
+    src: "../images/main9/rentiantang.png",
+  },
+  {
+    name: "奇怪的美国",
+    href: "http://v.youku.com/v_show/id_XNDcxNTgxODg4.html",
+    src: "../images/main9/meiguo.png",
+  },
+  {
+    name: "奇怪的日本",
+    href: "http://v.youku.com/v_show/id_XMTYzOTE3NDcy.html",
+    src: "../images/main9/qiguaideriben.png",
+  },
+  {
+    name: "超级演说家",
+    href: "http://video.baidu.com/v?word=%B3%AC%BC%B6%D1%DD%CB%B5%BC%D2&fr=ala9&ct=301989888&rn=20&pn=0&db=0&s=8",
+    src: "../images/main9/chaojiyanshuojia.png",
+  },
+  {
+    name: "我们结婚吧",
+    href: "http://www.soku.com/search_video/q_我们结婚吧?f=hint",
+    src: "../images/main9/womenjiehunba.png",
+  },
+  {
+    name: "两天一夜",
+    href: "http://www.youku.com/show_page/id_z41c1fe08146111e3a705.html",
+    src: "../images/main9/2tian1ye.png",
+  },
+  {
+    name: "泡芙小姐",
+    href: "http://www.youku.com/show_page/id_zb758bfbec1e911e2b16f.html",
+    src: "../images/main9/paofuxiaojie.png",
+  },
+  {
+    name: "爸爸去哪儿",
+    href: "http://v.360.cn/va/aMMpaaRw7Jc2DD.html",
+    src: "../images/main9/babaqunar.png",
+  },
+  {
+    name: "绝命毒师",
+    href: "http://v.360.cn/tv/Q4ZobqCoRmXnNn.html",
+    src: "../images/main9/juemingdushi.png",
+  },
+  {
+    name: "三国热",
+    href: "http://v.360.cn/tv/Q4htb3SrTGbnN3.html",
+    src: "../images/main9/sanguore.png",
+  },
+  {
+    name: "万万没想到",
+    href: "http://www.youku.com/show_page/id_zd1d065eafb1411e2a705.html",
+    src: "../images/main9/wanwanmeixiangdao.png",
+  },
+  {
+    name: "悦食中国",
+    href: "http://www.youku.com/show_page/id_z3b5b77e05bf811e3b8b7.html",
+    src: "../images/main9/yueshizhongguo.png",
+  },
+  {
+    name: "屌丝男士第三季",
+    href: "http://www.soku.com/detail/show/XMTE0NzU2OA==",
+    src: "../images/main9/diaosinanshi3.png",
+  },
+  {
+    name: "爱情公寓",
+    href: "http://v.baidu.com/v?word=%B0%AE%C7%E9%B9%AB%D4%A2&pids=20619_18075_10439_10438&vtype=tv",
+    src: "../images/main9/aiqinggongyu.jpg",
+  },
+  {
+    name: "大话西游-小宝说书",
+    href: "http://v.qq.com/vplus/meiriyijiong/foldervideos/y6200000132czvg",
+    src: "../images/main9/dahuaxiyou.jpg",
+  },
+];
