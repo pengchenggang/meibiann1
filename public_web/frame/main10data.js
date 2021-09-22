09afa89226de9b9191afa756f0dbd4632072aa84
彭成刚 <76526211@qq.com>
2021-09-22 19:10:14 +0800

    up

---
+++ public_web/frame/main10data.js
@@ -0,0 +1,172 @@
+var list = [
+  {
+    name: "jq22",
+    href: "http://www.jq22.com/",
+    src: "../images/main10/jq22.jpg",
+  },
+  {
+    name: "doyoudo",
+    href: "http://www.doyoudo.com/",
+    src: "../images/main10/doyoudo.jpg",
+  },
+  {
+    name: "swiper3中文网",
+    href: "http://www.swiper.com.cn/",
+    src: "../images/main10/swiper3.jpg",
+  },
+  {
+    name: "jquery之家",
+    href: "http://www.htmleaf.com/",
+    src: "../images/main10/jqHome.jpg",
+  },
+  {
+    name: "龋齿一号 素材分享",
+    href: "http://www.gfxcamp.com/",
+    src: "../images/main10/gfxcamp.jpg",
+  },
+  {
+    name: "jsfoot脚本特效",
+    href: "http://www.jsfoot.com/jquery/",
+    src: "../images/main10/jsfoot.png",
+  },
+  {
+    name: "css3html5效果库",
+    href: "http://zaole.net/",
+    src: "../images/main10/css3html5.png",
+  },
+  {
+    name: "sinaJs效果库",
+    href: "http://ln.sina.com.cn/zt/assist/index.html",
+    src: "../images/main10/sinajsku.png",
+  },
+  {
+    name: "网易js库 NEJ",
+    href: "http://nej.netease.com/",
+    src: "../images/main10/nej.png",
+  },
+  {
+    name: "50种华丽镜头光晕",
+    href: "http://www.uisdc.com/50-free-high-resolution-transparent-images",
+    src: "../images/main10/50guangyun.png",
+  },
+  {
+    name: "AK大神 AE教程",
+    href: "http://www.videocopilot.net/tutorials/",
+    src: "../images/main10/akdashen.png",
+  },
+  {
+    name: "jquery10个插件",
+    href: "http://www.cnblogs.com/lhb25/p/10-new-popular-jquery-plugins-check.html",
+    src: "../images/main10/jquery10plus.png",
+  },
+  {
+    name: "mts m2ts转换器",
+    href: "http://www.portablesoft.org/free-hd-converter/",
+    src: "../images/main10/mtszhuanhuan.png",
+  },
+  {
+    name: "历史剧.Net",
+    href: "http://www.lishiju.net/",
+    src: "../images/main10/lishiju.png",
+  },
+  {
+    name: "快手AAuto开发环境",
+    href: "http://www.aau.cn/",
+    src: "../images/main10/kuaishou.png",
+  },
+  {
+    name: "多说-评论系统",
+    href: "http://duoshuo.com/",
+    src: "../images/main10/duoshuo.png",
+  },
+  {
+    name: "dnspod域名管理",
+    href: "https://www.dnspod.cn/",
+    src: "../images/main10/dnspod.png",
+  },
+  {
+    name: "说吧-免费打电话",
+    href: "https://www.shuobar.cn/",
+    src: "../images/main10/shuobar.png",
+  },
+  {
+    name: "广告管家3",
+    href: "http://www.ad-safe.com/",
+    src: "../images/main10/adguanjia3.png",
+  },
+  {
+    name: "电商导航",
+    href: "http://hao.uisdc.com/shop/",
+    src: "../images/main10/dianshangNavi.png",
+  },
+  {
+    name: "免费画廊",
+    href: "http://www.freebiesgallery.com/",
+    src: "../images/main10/freehualang.png",
+  },
+  {
+    name: "wps25周年 时光巴士",
+    href: "http://www.wps.cn/zt/25year/",
+    src: "../images/main11/wps25year.png",
+  },
+  {
+    name: "南京2012创新峰会",
+    href: "http://nj.house.sina.com.cn/zhuanti/njxlljcxfh/index.shtml",
+    src: "../images/main11/xingdongdeliliang.png",
+  },
+  {
+    name: "360专题板式",
+    href: "http://sh.qihoo.com/zt/lkqcfdmsg.html",
+    src: "../images/main11/360ztbanshi.png",
+  },
+  {
+    name: "封尚人物专题板式",
+    href: "http://ent.qq.com/zt2013/bigstar69/index.htm",
+    src: "../images/main11/fengshangrenwu.png",
+  },
+  {
+    name: "公开课2013年终专题",
+    href: "http://open.163.com/2014/",
+    src: "../images/main11/2013yearall163.png",
+  },
+  {
+    name: "美编N1搜索",
+    href: "http://sinaleju.sinaapp.com/jSearch/",
+    src: "../images/main11/n1sousuo.png",
+  },
+  {
+    name: "楼盘搜索",
+    href: "http://tj.house.sina.com.cn/scan/loushihuangye2014/index.html",
+    src: "../images/main11/loupansousuo.png",
+  },
+  {
+    name: "慕课网 视频教学",
+    href: "http://www.imooc.com/course/list",
+    src: "../images/main10/mukewang.jpg",
+  },
+  {
+    name: "有字库 加载网页字体",
+    href: "http://www.youziku.com/",
+    src: "../images/main10/youziku.jpg",
+  },
+  {
+    name: "思源黑体",
+    href: "http://dl.iplaysoft.com/files/2855.html",
+    src: "../images/main10/siyuanheiti.jpg",
+  },
+  {
+    name: "淘宝CodeSvn",
+    href: "http://code.taobao.org/",
+    src: "../images/main10/taocode.jpg",
+  },
+  {
+    name: "小丸工具箱 视频转码",
+    href: "http://maruko.appinn.me/",
+    src: "../images/main10/xiaowan.jpg",
+  },
+  {
+    name: "硬盘双击打不开",
+    href: "http://ask.zol.com.cn/q/6296.html",
+    src: "../images/main10/diskgenius.jpg",
+  },
+];
