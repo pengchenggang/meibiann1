649d2b0ea0c6cec87491b0d2a61f0638ff484e17
彭成刚 <76526211@qq.com>
2021-09-22 19:28:03 +0800

    up

---
+++ public_web/frame/main12data.js
@@ -0,0 +1,407 @@
+var list = [
+  {
+    name: "xzj-1 电商流程X展架",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-1.jpg",
+  },
+  {
+    name: "xzj-2 乐居贷X展架",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-2.jpg",
+  },
+  {
+    name: "xzj-3 电商优惠资质X展架",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-3.jpg",
+  },
+  {
+    name: "xzj-4 优惠X展架（不带资质）",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-4.jpg",
+  },
+  {
+    name: "xzj-5 微博宣传易拉宝",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-5.jpg",
+  },
+  {
+    name: "xzj-6 看房团免费招募易拉宝",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-6.jpg",
+  },
+  {
+    name: "xzj-7 微博X展架80-180",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-7.jpg",
+  },
+  {
+    name: "xzj-8 微直播",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-8.jpg",
+  },
+  {
+    name: "xzj-9 新浪乐居易拉宝 邓伟俊 深圳设计",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-9.jpg",
+  },
+  {
+    name: "xzj-10 微博平面稿竖版-蓝",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-10.jpg",
+  },
+  {
+    name: "xzj-11 乐居会招募X展架 一睿 杨亮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-11.jpg",
+  },
+  {
+    name: "xzj-12 2014年睿士堂X展架 范晓红 杨亮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-12.jpg",
+  },
+  {
+    name: "xzj-13 z在现场X展架 范晓红 杨亮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-13.jpg",
+  },
+  {
+    name: "xzj-14 如果你知道X展架 黄建文 杨亮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-14.jpg",
+  },
+  {
+    name: "xzj-15 云对话X展架 郭小涛 杨亮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-15.jpg",
+  },
+  {
+    name: "xzj-16 名博品鉴团X展架 郭晓涛 王昊",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-16.jpg",
+  },
+  {
+    name: "xzj-17 首届滨海移动购房节x展架 胡津玮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-17.jpg",
+  },
+  {
+    name: "xzj-18 口袋乐居X展架 王昊",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-18.jpg",
+  },
+  {
+    name: "xzj-19 指尖上的世界杯X展架 杨亮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-19.jpg",
+  },
+  {
+    name: "xzj-20 乐居青苗X展架 张秩维",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-20.jpg",
+  },
+  {
+    name: "xzj-21 微信购房季X展架 杨亮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-21.jpg",
+  },
+  {
+    name: "xzj-22 保利诺丁山翻翻乐 王昊",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-22.jpg",
+  },
+  {
+    name: "xzj-23 金羊迎春乐居大吉X展架 王昊",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-23.jpg",
+  },
+  {
+    name: "xzj-24 乐居运动会X展架 王昊",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-24.jpg",
+  },
+  {
+    name: "xzj-25 乐居携手津房置换X展架 王昊",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-25.jpg",
+  },
+  {
+    name: "xzj-26 码上专车 大易拉宝",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-26.jpg",
+  },
+  {
+    name: "xzj-27 凯撒旅游易拉宝",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-27.jpg",
+  },
+  {
+    name: "xzj-28 金隅满堂 电商X展架 新 刘丽颖",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-28.jpg",
+  },
+  {
+    name: "xzj-29 乐居摇号规则易拉宝 刘丽颖",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-29.jpg",
+  },
+  {
+    name: "xzj-30 乐居大集易拉宝 刘丽颖",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-30.jpg",
+  },
+  {
+    name: "xzj-31 看房团易拉宝 总部",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-31.jpg",
+  },
+  {
+    name: "xzj-32 乐居书友会 易拉宝 王昊",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-32.jpg",
+  },
+  {
+    name: "xzj-33 写字楼 邻居日 80-180 郑连庆 张秩维",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-33.jpg",
+  },
+  {
+    name: "xzj-34 乐企汇 刘玉良 刘丽颖",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-34.jpg",
+  },
+  {
+    name: "xzj-35 摇号现场易拉宝 刘丽颖",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-35.jpg",
+  },
+  {
+    name: "xzj-36 G观锦大集X展架 张秩维",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-36.jpg",
+  },
+  {
+    name: "xzj-37 电商最新X展架 张秩维20161115",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/xzj-37.jpg",
+  },]
+  var list2 = [
+  {
+    name: "dm-1 20140512红桥专场看房团DM",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-1.jpg",
+  },
+  {
+    name: "dm-1 20140512红桥专场看房团DM背面",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-1B.jpg",
+  },
+  {
+    name: "dm-2 乐居会DM",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-2.jpg",
+  },
+  {
+    name: "dm-3 看房团DM单 正面",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-3.jpg",
+  },
+  {
+    name: "dm-3 看房团DM单 背面",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-3B.jpg",
+  },
+  {
+    name: "dm-4 地图z 20140519 印刷 840-570mm",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-4.jpg",
+  },
+  {
+    name: "dm-5 口袋乐居杂志 221-284mm 高静",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-5.jpg",
+  },
+  {
+    name: "dm-6 乐居品牌广告－手指篇",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-6.jpg",
+  },
+  {
+    name: "dm-7 乐居品牌广告－二手房",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-7.jpg",
+  },
+  {
+    name: "dm-8 码上专车红包海报",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-8.jpg",
+  },
+  {
+    name: "dm-8 码上专车红包海报B",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/dm-8B.jpg",
+  },
+]
+var list3 = [
+  {
+    name: "bb-1 电商背板 307cm-232cm",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/bb-1.jpg",
+  },
+  {
+    name: "bb-2 2014春季房交会背景板6-4m 李航",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/bb-2.jpg",
+  },
+  {
+    name: "bb-3 蝶变 背板 300-220cm",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/bb-3.jpg",
+  },
+  {
+    name: "bb-4 招商依云郡1.6-2m 项目地图 刘燕宁",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/bb-4.jpg",
+  },
+  {
+    name: "bb-5 迷你嘉年华 4-3m",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/bb-5.jpg",
+  },
+  {
+    name: "bb-6 乐居圣诞趴 让你HIGH不停 3-2m",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/bb-6.jpg",
+  },
+  {
+    name: "bb-7 乐企汇 羽毛球竞技赛",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/bb-7.jpg",
+  },]
+  var list4 = [
+  {
+    name: "qita-1 乐居+U站 首页横切 背景源文件",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/qita-1.jpg",
+  },
+  {
+    name: "qita-2 天津新浪乐居设计部logo",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/qita-2.jpg",
+  },
+  {
+    name: "qita-3 天津新浪乐居官方微信使用手册",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/qita-3.jpg",
+  },
+  {
+    name: "qita-4 91实惠H5封皮",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/qita-4.jpg",
+  },
+  {
+    name: "qita-5 乐居房探 logo",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/qita-5.jpg",
+  },
+  {
+    name: "qita-6 大悦城LED屏",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/qita-6.jpg",
+  },
+  {
+    name: "qita-7 美编N1新logo",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/qita-7.jpg",
+  },]
+  var list5 = [
+  {
+    name: "图片晶块化 poly软件",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main2/poly.jpg",
+  },
+  {
+    name: "flash改链接软件",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-2.jpg",
+  },
+  {
+    name: "swf2flv 曦力",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-3.jpg",
+  },
+  {
+    name: "rj-5 GifCam_4.0",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-5.jpg",
+  },
+  {
+    name: "rj-6 jpg2ai",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-6.jpg",
+  },
+  {
+    name: "rj-7 图片堆积器",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-7.jpg",
+  },
+  {
+    name: "rj-8 swf下载",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-8.jpg",
+  },
+  {
+    name: "rj-9 流程图 Visio",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-9.jpg",
+  },
+  {
+    name: "rj-10 winhotkey",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-10.jpg",
+  },
+  {
+    name: "rj-11 文本转音频",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-11.jpg",
+  },
+  {
+    name: "rj-12 PTGui Pro",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-12.jpg",
+  },
+  {
+    name: "rj-13 tinypng 压缩",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-13.jpg",
+  },
+  {
+    name: "rj-14 批量修改文件名",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-14.jpg",
+  },
+  {
+    name: "rj-15 pdf2doc",
+    href: "http://pan.baidu.com/s/1sjpr5rn",
+    src: "../images/main12/rj-15.jpg",
+  },
+  {
+    name: "rj-16 everything",
+    href: "http://www.voidtools.com/",
+    src: "../images/main12/rj-16.jpg",
+  },
+  {
+    name: "rj-17 Knoll Light",
+    href: "http://www.voidtools.com/",
+    src: "../images/main12/rj-17.jpg",
+  },
+  {
+    name: "rj-18 PATH1.4",
+    href: "http://www.uisdc.com/font-ai",
+    src: "../images/main12/rj-18.jpg",
+  },
+  {
+    name: "rj-19 磨皮滤镜",
+    href: "https://www.baidu.com/s?wd=Portraiture",
+    src: "../images/main12/rj-19.jpg",
+  },
+];
