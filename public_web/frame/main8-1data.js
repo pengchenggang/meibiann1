7c6c6d318e89d163f3571b4dfe4dd93985700daf
彭成刚 <76526211@qq.com>
2021-09-22 18:49:14 +0800

    up

---
+++ public_web/frame/main8-1data.js
@@ -0,0 +1,342 @@
+var list = [
+  {
+    name: "2014年专题总结",
+    href: "main8.html",
+    src: "../images/main8-1/2014icon-n1.png",
+  },
+  {
+    name: "2015看房团",
+    href: "http://tj.house.sina.com.cn/zhuanti/kanfangtuan/",
+    src: "../images/main8-1/kanfangtuan2015.jpg",
+  },
+  {
+    name: "2014电商总结",
+    href: "http://tj.house.sina.com.cn/zhuanti/2014dianshangnianzhongzongjie/",
+    src: "../images/main8-1/2014dianshangzongjie.jpg",
+  },
+  {
+    name: "2014电商H5总结",
+    href: "http://tj.house.sina.com.cn/mobile/dsndzj/",
+    src: "../images/main8-1/2014dianshangzongjieH5.png",
+  },
+  {
+    name: "2014数据400总结",
+    href: "http://tj.house.sina.com.cn/zhuanti/2014NZPD400pian/",
+    src: "../images/main8-1/2014shujuzongjie.jpg",
+  },
+  {
+    name: "2014榜样楼盘",
+    href: "http://tj.house.sina.com.cn/zhuanti/2014dljjpzt/",
+    src: "../images/main8-1/2014bangyangloupan.jpg",
+  },
+  {
+    name: "诺丁山点鞭炮PC",
+    href: "http://tj.house.sina.com.cn/zhuanti/2015nuodingshandianbianpao/",
+    src: "../images/main8-1/ndsdianbianpaoPC.jpg",
+  },
+  {
+    name: "诺丁山点鞭炮H5",
+    href: "http://tj.house.sina.com.cn/mobile/ndsdianbianpaoH5/index.html",
+    src: "../images/main8-1/ndsdianbianpaoH5.jpg",
+  },
+  {
+    name: "无辜的背叛PC",
+    href: "http://tj.house.sina.com.cn/zhuanti/2015spwgdbp/",
+    src: "../images/main8-1/wgdbp2015pc.jpg",
+  },
+  {
+    name: "无辜的背叛H5",
+    href: "http://tj.house.sina.com.cn/mobile/2015wugudebeipanH5/",
+    src: "../images/main8-1/wgdbp2015H5.jpg",
+  },
+  {
+    name: "400月报",
+    href: "http://tj.house.sina.com.cn/zhuanti/1502/400dianhua1yueshujufenxi/",
+    src: "../images/main8-1/400laidian.jpg",
+  },
+  {
+    name: "2015 315说",
+    href: "http://tj.house.sina.com.cn/zhuanti/1502/315qianwangoufangzheshuo.html",
+    src: "../images/main8-1/2015315.jpg",
+  },
+  {
+    name: "五矿榕园品牌馆",
+    href: "http://tj.house.sina.com.cn/zhuanti/wkry/",
+    src: "../images/main8-1/wukuangrongyuanppg.jpg",
+  },
+  {
+    name: "中国铁建国际城云购H5",
+    href: "http://tj.house.sina.com.cn/mobile/ztjgjcyg/",
+    src: "../images/main8-1/tiejianguojichengyungou.jpg",
+  },
+  {
+    name: "400品宣",
+    href: "http://tj.house.sina.com.cn/zhuanti/fcbst400/",
+    src: "../images/main8-1/400pinxuan.jpg",
+  },
+  {
+    name: "爱的罚单 曾蕊",
+    href: "http://tj.house.sina.com.cn/zhuanti/1503baoliaidefadan/",
+    src: "../images/main8-1/aidefadan.jpg",
+  },
+  {
+    name: "2015滨海房地产交易会",
+    href: "http://tj.house.sina.com.cn/zhuanti/bhfjh2015/",
+    src: "../images/main8-1/2015binhaijiaoyihui.jpg",
+  },
+  {
+    name: "滨海房房交会H5 带分享",
+    href: "http://tj.house.sina.com.cn/scan/app/bhfjh2015.html",
+    src: "../images/main8-1/2015binhaifangjiaohuiH5.jpg",
+  },
+  {
+    name: "格调品牌馆2015 pc",
+    href: "http://tj.house.sina.com.cn/zhuanti/gediaopinpai/",
+    src: "../images/main8-1/gediaopc2015.jpg",
+  },
+  {
+    name: "格调品牌馆2015 H5",
+    href: "http://tj.house.sina.com.cn/mobile/gdsn2015/",
+    src: "../images/main8-1/gediaoH5.jpg",
+  },
+  {
+    name: "湿地公园投票PC",
+    href: "http://tj.house.sina.com.cn/zhuanti/2015tjParkVote/",
+    src: "../images/main8-1/shiditoupiaoPc.jpg",
+  },
+  {
+    name: "湿地公园投票H5",
+    href: "http://tj.house.sina.com.cn/mobile/2015tjParkVote/",
+    src: "../images/main8-1/shiditoupiaoH5.jpg",
+  },
+  {
+    name: "融创绿城报名H5",
+    href: "http://tj.house.sina.com.cn/zhuanti/rclvqycbm2015/",
+    src: "../images/main8-1/rongchuanglvchengbaoming.jpg",
+  },
+  {
+    name: "乐居大集H5",
+    href: "http://tj.house.sina.com.cn/mobile/ljdj2015/",
+    src: "../images/main8-1/lejudajiH5.jpg",
+  },
+  {
+    name: "途家赏花住途家",
+    href: "http://tj.house.sina.com.cn/zhuanti/tachunshanghuazhutujia/",
+    src: "../images/main8-1/tujia.jpg",
+  },
+  {
+    name: "途家津城周边游",
+    href: "http://tj.house.sina.com.cn/zhuanti/tujiataqinghuanbohai/",
+    src: "../images/main8-1/tujia2.jpg",
+  },
+  {
+    name: "400品宣 H5",
+    href: "http://tj.house.sina.com.cn/mobile/lj400px2015/",
+    src: "../images/main8-1/400pinxuanH5.jpg",
+  },
+  {
+    name: "电商品宣 H5",
+    href: "http://tj.house.sina.com.cn/mobile/lejuds2015/",
+    src: "../images/main8-1/dianshangpinxuanH5.jpg",
+  },
+  {
+    name: "乐居大集 PC",
+    href: "http://tj.house.sina.com.cn/zhuanti/lejudaji/",
+    src: "../images/main8-1/lejudajiPc.jpg",
+  },
+  {
+    name: "旺海公府骑行",
+    href: "http://tj.house.sina.com.cn/zhuanti/wanghaigongfuqixing/",
+    src: "../images/main8-1/wanghaigongfuqixing.jpg",
+  },
+  {
+    name: "印象天津",
+    href: "http://tj.house.sina.com.cn/zhuanti/yinxiangtianjin/",
+    src: "../images/main8-1/yinxiangtianjin.jpg",
+  },
+  {
+    name: "一分钟学区房",
+    href: "http://t.cn/RA1JkFX",
+    src: "../images/main9/xuequfang.jpg",
+  },
+  {
+    name: "电商51H5",
+    href: "http://tj.house.sina.com.cn/mobile/dswyldj2015/",
+    src: "../images/main8-1/dianshang51H5.jpg",
+  },
+  {
+    name: "旭辉御园移动视频",
+    href: "http://tj.house.sina.com.cn/mobile/2015xuhuiyufuweiguanggao/",
+    src: "../images/main8-1/xuhuiyufuMobileVideo.jpg",
+  },
+  {
+    name: "融科瀚棠·实惠",
+    href: "http://tj.house.sina.com.cn/zhuanti/wuyueshihuifengqianggendu/",
+    src: "../images/main8-1/rongkehantangshihui.jpg",
+  },
+  {
+    name: "东方环球影城专题",
+    href: "http://tj.house.sina.com.cn/zhuanti/dfhqychf7y/",
+    src: "../images/main8-1/dongfanghuanqiuyingcheng.jpg",
+  },
+  {
+    name: "不占便宜（天津实惠）",
+    href: "http://t.cn/R2Iqk2x",
+    src: "../images/main8-1/buzhanpianyi.jpg",
+  },
+  {
+    name: "保利实惠",
+    href: "http://tj.house.sina.com.cn/zhuanti/6yblsh2015/",
+    src: "../images/main8-1/shihuibaoli.jpg",
+  },
+  {
+    name: "91乐居卡",
+    href: "http://tj.house.sina.com.cn/zhuanti/91lejuka20150615/",
+    src: "../images/main8-1/91lejuka.jpg",
+  },
+  {
+    name: "宜禾澜岸车位摇号",
+    href: "http://tj.house.sina.com.cn/zhuanti/yihelananyaohao20150617/",
+    src: "../images/main8-1/yihelananyaohao20150617.jpg",
+  },
+  {
+    name: "走进天津-投资天津",
+    href: "http://tj.house.sina.com.cn/zhuanti/zoujintianjin2015/",
+    src: "../images/main8-1/zoujintianjin.jpg",
+  },
+  {
+    name: "东方环球影城-实惠",
+    href: "http://tj.house.sina.com.cn/zhuanti/dfhqychf7y/",
+    src: "../images/main8-1/yingchengShihui.jpg",
+  },
+  {
+    name: "格调林泉-快递",
+    href: "http://tj.house.sina.com.cn/zhuanti/huozhuolinquankuaidixiaoge/",
+    src: "../images/main8-1/linquankuaidi.jpg",
+  },
+  {
+    name: "格调林泉摇号公示",
+    href: "http://tj.house.sina.com.cn/zhuanti/gediaolinquanyaohaogongshi/",
+    src: "../images/main8-1/linquanyaohao.jpg",
+  },
+  {
+    name: "毕业买房季 原创",
+    href: "http://t.cn/RLp5ZwG",
+    src: "../images/main8-1/biyemaifangji.jpg",
+  },
+  {
+    name: "中铁粮运速递",
+    href: "http://tj.house.sina.com.cn/zhuanti/zhongtieyunliangsudikaimendaji/",
+    src: "../images/main8-1/zhongtieliangyun.jpg",
+  },
+  {
+    name: "2015你该学习AE了",
+    href: "http://tj.house.sina.com.cn/zhuanti/2015nigaixuexiaele/",
+    src: "../images/main8-1/2015xueae.jpg",
+  },
+  {
+    name: "码上专车",
+    href: "http://tj.house.sina.com.cn/zhuanti/mszc20150806/",
+    src: "../images/main8-1/mashangzhuanche.jpg",
+  },
+  {
+    name: "码上专车-移动版",
+    href: "http://tj.house.sina.com.cn/mobile/mszc150813/",
+    src: "../images/main8-1/mashangzhuancheyidong.jpg",
+  },
+  {
+    name: "91乐居卡",
+    href: "http://tj.house.sina.com.cn/zhuanti/91leju150819/",
+    src: "../images/main8-1/91leju.jpg",
+  },
+  {
+    name: "全民砍价师",
+    href: "http://tj.house.sina.com.cn/zhuanti/qmkjs2015/",
+    src: "../images/main8-1/quanminkanjiashi.jpg",
+  },
+  {
+    name: "电商约会单身汪",
+    href: "http://tj.house.sina.com.cn/zhuanti/dsyhdsw2015/",
+    src: "../images/main8-1/dianshangdsw.jpg",
+  },
+  {
+    name: "扫忙帮征集令",
+    href: "http://tj.house.sina.com.cn/zhuanti/saomangbangzhengjiling/",
+    src: "../images/main8-1/saomangbang.jpg",
+  },
+  {
+    name: "大悦城疯抢节",
+    href: "http://tj.house.sina.com.cn/zhuanti/dayuechengfengqiangjie/",
+    src: "../images/main8-1/dayuechengfff.jpg",
+  },
+  {
+    name: "秋&middot;团圆 H5",
+    href: "http://tj.house.sina.com.cn/mobile/qiutuanyuan150914/",
+    src: "../images/main8-1/2015zhongqiu.jpg",
+  },
+  {
+    name: "LEJU电商料理新生活",
+    href: "http://m.leju.com/?site=touch&ctl=topic&act=news_topic_info&city=tj&id=6054440962728703904",
+    src: "../images/main8-1/dianshang9yueH5.jpg",
+  },
+  {
+    name: "楼盘推荐",
+    href: "http://m.leju.com/?site=touch&ctl=topic&act=news_topic_info&city=tj&id=6054548604688054247",
+    src: "../images/main8-1/tuijianloupan.jpg",
+  },
+  {
+    name: "重阳节H5",
+    href: "http://m.leju.com/?site=touch&ctl=topic&act=news_topic_info&city=tj&id=6062129830286374437",
+    src: "../images/main8-1/chongyangjie.jpg",
+  },
+  {
+    name: "最视觉2015 PC",
+    href: "http://tj.house.sina.com.cn/zhuanti/zuishijuan2015mb/",
+    src: "../images/main8-1/zuishijue2015.jpg",
+  },
+  {
+    name: "乐居展位图-移动",
+    href: "http://m.leju.com/?site=touch&ctl=topic&act=news_topic_info&city=tj&id=6062513956592072574",
+    src: "../images/main8-1/leju2015zhanweitu.jpg",
+  },
+  {
+    name: "电商10月H5品宣",
+    href: "http://m.leju.com/?site=touch&ctl=topic&act=news_topic_info&city=tj&id=6064582044850370097",
+    src: "../images/main8-1/dianshang201510.png",
+  },
+  {
+    name: "实惠品牌商家联盟",
+    href: "http://tj.house.sina.com.cn/zhuanti/shppsjlm/",
+    src: "../images/main8-1/shihuilianmeng.jpg",
+  },
+  {
+    name: "金隅悦城码上专车",
+    href: "http://tj.house.sina.com.cn/zhuanti/pinzhiweijinyuyuequancheng/",
+    src: "../images/main8-1/jinyumszc.jpg",
+  },
+  {
+    name: "胎动危机谁来拯救",
+    href: "http://m.leju.com/?site=touch&ctl=topic&act=news_topic_info&city=tj&id=6075961636987123273",
+    src: "../images/main8-1/taidongweiji.jpg",
+  },
+  {
+    name: "2015年终盘点楼盘",
+    href: "http://tj.house.sina.com.cn/zhuanti/2015loupanpandian/",
+    src: "../images/main8-1/pandian2015.jpg",
+  },
+  {
+    name: "解读N01 码上专车",
+    href: "http://t.cn/R4LSn3O",
+    src: "../images/main8-1/jdN01.jpg",
+  },
+  {
+    name: "2015电商总结-移动",
+    href: "http://tj.house.sina.com.cn/zhuanti/zjds2015kjtt/",
+    src: "../images/main8-1/2015dianshangzj.jpg",
+  },
+  {
+    name: "2015一块调色板-商编",
+    href: "http://tj.leju.com/zhuanti/yikuaitiaoseban/",
+    src: "../images/main8-1/tiaoseban.jpg",
+  },
+];
